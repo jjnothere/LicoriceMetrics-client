be06c9eaa28e9b0f58326def42fb3807d5f575d6
Jeremy Johnson <jjnothere@gmail.com>
2025-05-07 22:32:27 -0500

    refresh token working local and for profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -25,29 +25,22 @@
   const { isLoggedIn, user, checkAuthStatus } = useAuth();
   const adAccounts = ref([]); // Store ad account IDs and names
   
-  // Helper function to retrieve the token from cookies
-  const getTokenFromCookies = () => {
-    const cookie = document.cookie.split('; ').find(row => row.startsWith('accessToken='));
-    return cookie ? cookie.split('=')[1] : null;
-  };
   
   // Fetch User Profile and Ad Account Names
   const fetchUserProfile = async () => {
-    const token = getTokenFromCookies();
     try {
       const response = await api.get('/user-profile', {
-        headers: { Authorization: `Bearer ${token}` },
         withCredentials: true
       });
-  
+
       // Update user profile details
       user.firstName = response.data.firstName;
       user.lastName = response.data.lastName;
-  
+
       // Fetch ad account names based on ad account IDs
       const accountsWithNames = await fetchAdAccountNames(response.data.adAccounts);
       adAccounts.value = accountsWithNames;
-  
+
     } catch (error) {
       console.error('Error fetching user profile:', error);
     }
@@ -55,19 +48,17 @@
   
   // Fetch Ad Account Names
   const fetchAdAccountNames = async (adAccounts) => {
-    const token = getTokenFromCookies();
     try {
       const response = await api.get('/ad-account-name', {
-        headers: { Authorization: `Bearer ${token}` },
         withCredentials: true
       });
-  
+
       // Assuming response.data.adAccounts contains all the account data
       const accountNames = response.data.adAccounts.map(account => ({
         id: account.id,
         name: account.name || 'Unknown Account',
       }));
-  
+
       return accountNames;
     } catch (error) {
       console.error('Error fetching ad account names:', error);
@@ -81,8 +72,10 @@
   // Lifecycle hook to check auth status and fetch profile if authenticated
   onMounted(() => {
     checkAuthStatus();
-    if (isLoggedIn.value) {
-      fetchUserProfile();
+    if (!isLoggedIn.value) {
+      router.push('/');
+      return;
     }
+    fetchUserProfile();
   });
   </script>
